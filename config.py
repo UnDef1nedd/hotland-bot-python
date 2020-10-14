e83d86ffe82a317960c8005dfa47addd7446f44b
HotBan <misha@z370.net>
2020-10-14 20:42:58 +0300

    1

---
+++ config.py
@@ -0,0 +1,8 @@
+settings = {
+    'TOKEN': 'NzY1NjM1MjU5Mjg2MDI4MzE4.X4XrZg.qD-ATbyTqup2CnIErbSh_RlrXUQ', # Получение токена - https://prnt.sc/uofhur
+    'NAME BOT': 'HOTLAND', # Имя вашего бота
+    'ID': '765635259286028318', # Получение ID - https://prnt.sc/uofik2
+    'PREFIX': '>', # Префикс
+    'OWNER':'HotBan#7777',    # ИЗМЕНЯЙТЕ НА СВОЙ NICK:ID (Пример: Toil#5605)
+    'OWNER NAME':'HotBan' # ИМЯ ВЛАДЕЛЬЦА (Пример: Toil)
+}
